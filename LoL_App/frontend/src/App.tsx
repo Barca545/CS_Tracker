c6e41841d3bf3da9c63745bd1ba032b431b03287
Barca545 <124243414+Barca545@users.noreply.github.com>
2023-04-15 23:08:55 -0400

    Fixed backend

---
+++ LoL_App/frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import SearchMatch from './components/search'
+import DisplayMatches from './components/match-display'
+import Results from './components/results'
+
+function App() {
+  return (
+    <div className="App">
+        <div className='header'>
+          <SearchMatch/>
+        </div>
+        <div className='left'/>
+        <div className='middle'>
+          <DisplayMatches/>
+        </div>
+        <div className='right'/>
+        <div className='footer'>
+          <Results/>
+        </div>
+    </div>
+  );
+}
+export default App;
---
+++ LoL_App/frontend/src/components/results.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './components-styles.css';
+ 
+
+export default function Results(){
+    return(
+        <div className='results-wrapper'>
+           <div className='results-header'>Results</div>
+           <div className='cs-15'> CS@15: </div>
+           <div className='problem-cs'>Problem Minutes: </div>
+           <div className='results-graph'></div>
+        </div>
+    )
+}
