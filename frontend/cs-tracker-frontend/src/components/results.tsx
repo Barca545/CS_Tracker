925f2d05531d3b5c794a7f2ba18381d0023d9291
Barca545 <124243414+Barca545@users.noreply.github.com>
2023-04-15 20:22:04 -0400

    added results section

--- frontend/cs-tracker-frontend/src/components/results.tsx
+++ frontend/cs-tracker-frontend/src/components/results.tsx
@@ -6,7 +6,9 @@ export default function Results(){
     return(
         <div className='results-wrapper'>
            <div className='results-header'>Results</div>
-           <div className='results-graph-wrapper'></div>
+           <div className='cs-15'> CS@15: </div>
+           <div className='problem-cs'>Problem Minutes: </div>
+           <div className='results-graph'></div>
         </div>
     )
 }
