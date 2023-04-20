e100a7f68ba54d4d9dedd8f0e72d91608e014564
Basrca545 <jamarioneal@gmail.com>
2023-04-20 01:49:52 -0400

    Added dependencies

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jamar\Documents\Hobbies\Coding\CS_Tracker
+set VIRTUAL_ENV=C:\Users\Jamari\Documents\Hobbies\Coding\CS_Tracker
 
 if not defined PROMPT set PROMPT=$P$G
 
