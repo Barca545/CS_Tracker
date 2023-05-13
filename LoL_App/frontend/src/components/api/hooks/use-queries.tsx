5dce20fdc3341c4d96e40e4c7913bcfbf6eab746
Basrca545 <jamarioneal@gmail.com>
2023-05-12 21:31:35 -0400

    updated queries

--- LoL_App/frontend/src/components/api/hooks/use-queries.tsx
+++ LoL_App/frontend/src/components/api/hooks/use-queries.tsx
@@ -3,7 +3,7 @@ import {get} from '../fetchers/fetchers';
 import {MatchList,Match} from '../types/types';
 
 export function useGetMatchList(props:any){
-  const [matches, setMatches] = useState<Match[]>([]); /// possibly make this a hook in the App.tsx
+  const [ matches, setMatches] = useState<Match[]>([]); /// possibly make this a hook in the App.tsx
   
   const getMatches = async () => {
     const { list } = await get<MatchList>('/matchlist/Envoker/na1/1/');///test url
