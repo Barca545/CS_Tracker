6eedd66c3e8963627856724f75072a314fe5334f
Basrca545 <jamarioneal@gmail.com>
2023-05-15 04:34:54 -0400

    using context

--- LoL_App/frontend/src/App.tsx
+++ LoL_App/frontend/src/App.tsx
@@ -3,25 +3,21 @@ import './App.css';
 import SearchMatch from './components/search'
 import DisplayMatches from './components/match-display'
 import Results from './components/results'
-import {MatchesContext} from './contexts'
 
 function App() {
-  const matches = useContext(MatchesContext) /// this may need to just be converted back into a hook
   return (
     <div className="App">
-      <MatchesContext.Provider value={matches}>
-        <div className='header'>
-          <SearchMatch matches={matches}/>
-        </div>
-        <div className='left'/>
-        <div className='middle'>
-          <DisplayMatches matches={matches}/>
-        </div>
-        <div className='right'/>
-        <div className='footer'>
-          <Results/>
-        </div>
-      </MatchesContext.Provider>
+      <div className='header'>
+        <SearchMatch/>
+      </div>
+      <div className='left'/>
+      <div className='middle'>
+        <DisplayMatches/>
+      </div>
+      <div className='right'/>
+      <div className='footer'>
+        <Results/>
+      </div>
     </div>
   );
 }
