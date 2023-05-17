33a07c99a90e1a352d381b4e855c1c8916c10b2b
Basrca545 <jamarioneal@gmail.com>
2023-05-17 03:22:32 -0400

    rearranged how Redux works

--- LoL_App/frontend/src/app/store.tsx
+++ LoL_App/frontend/src/app/store.tsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {apiSlice} from '../services/apiSlice';
-import  matchlistrequestreducer from "../components/search/matchlistrequestSlice";
+import  matchlistrequestreducer from "../components/search/matchlistSlice";
 import { combineReducers } from '@reduxjs/toolkit'
 
 ///try installing a logger for easier debugging: https://github.com/LogRocket/redux-logger
@@ -16,3 +16,5 @@ const store =  configureStore({
  
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store
